28891bc15044b3fbdbb84efe411db61b4597c9b3
xiaofei_xi@163.com <xiaofei_xi@163.com>
2019-02-19 15:28:20 +0800

    FirstDemo 错删测试

---
+++ src/test/java/com/xl/AsciiPictest.java
@@ -0,0 +1,41 @@
+package com.xl;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by  X.L on 2018/8/13.
+ */
+public class AsciiPictest {
+
+    public static void createAsciiPic(final String path) {
+        final String base = "@#&$%*o!;.";// 字符串由复杂到简单
+        try {
+            final BufferedImage image = ImageIO.read(new File(path));
+            for (int y = 0; y < image.getHeight(); y += 2) {
+                for (int x = 0; x < image.getWidth(); x++) {
+                    final int pixel = image.getRGB(x, y);
+                    final int r = (pixel & 0xff0000) >> 16, g = (pixel & 0xff00) >> 8, b = pixel & 0xff;
+                    final float gray = 0.299f * r + 0.578f * g + 0.114f * b;
+                    final int index = Math.round(gray * (base.length() + 1) / 255);
+                    System.out.print(index >= base.length() ? " " : String.valueOf(base.charAt(index)));
+                }
+                System.out.println("我是TestBranch");
+            }
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * test
+     *
+     * @param args
+     */
+    public static void main(final String[] args) {
+        AsciiPictest.createAsciiPic("C:\\Users\\Administrator\\Desktop\\005.jpg");
+    }
+
+}
